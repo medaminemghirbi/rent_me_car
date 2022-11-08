e3f60644427bb83c8f4cb5ec6466df5e6bfb1189
medaminemghirbi <mohamed.amine.mghirbi@gmail.com>
2022-11-08 11:39:54 +0100

    second-commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,40 @@
 class SessionsController < ApplicationController
-    # include CurrentUserConcern
+     include CurrentUserConcern
    
      def create
        user = User
               .find_by(email: params['email'])
               .try(:authenticate, params['password'])
        if user
-         session[:user_id] = user.id
+         session[:user_id] = user.id  
          render json: {
            status: :created,
            logged_in: true,
            user_id: user.id ,
-           role: user.role,
+           role: user.role, 
            user: user
          }
        else
          render json: { status: 401 }
        end
      end
-   
-     def logout
-       reset_session
-       render json: { status: 200, logged_out: true }
-     end
+
+     def logged_in
+      
+      if @current_user
+        render json: {
+          logged_in: true,
+          user: @current_user
+        }, methods: [:user_image_url]
+      else
+        render json: {
+          logged_in: false
+        }
+      end
+    end
+
+    def logout
+      reset_session
+      render json: { status: 200, logged_out: true }
+    end
    end
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module RentCar
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: 'ren_me_car'
+    config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore, key: 'ren_me_car')
   end
 end
