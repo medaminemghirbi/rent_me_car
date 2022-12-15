0c62a3be881a1f5fb0caa81ef6acf66c031cb5b2
medaminemghirbi <mohamed.amine.mghirbi@gmail.com>
2022-12-15 11:07:08 +0100

    prepare to deploy

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,5 @@
 class StaticController < ApplicationController
     def home
-    render json: { status: "it's working" }
-      #render file: Rails.root.join('public', 'index.html')
-  
+      render file: Rails.root.join('public', 'index.html')
     end
-  end
+end
